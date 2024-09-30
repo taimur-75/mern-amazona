c39df537dcdb40e60bb1faf02ddecfce264840e5
Taimur Hossen <taimurhossen75@gmail.com>
2024-10-01 01:35:13 +0530

    Implement optional chaining for safer products mapping in HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -55,7 +55,7 @@ function HomeScreen() {
           <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row>
-            {products.map((product) => (
+            {products?.map((product) => (
               <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
                 <Product product={product}></Product>
               </Col>
